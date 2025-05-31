af5e50b568d4c7a0340ada6a354416ea6f3b800e
DanielDMani <info@delexity.com>
2025-05-31 20:07:43 +0100

    literallycreatedthexcodefolderhere
    push 1

---
+++ literallyxcodetestlol/literallyxcodetestlol/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  literallyxcodetestlol
+//
+//  Created by Daniel Mani on 31/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ literallyxcodetestlol/literallyxcodetestlolTests/literallyxcodetestlolTests.swift
@@ -0,0 +1,17 @@
+//
+//  literallyxcodetestlolTests.swift
+//  literallyxcodetestlolTests
+//
+//  Created by Daniel Mani on 31/05/2025.
+//
+
+import Testing
+@testable import literallyxcodetestlol
+
+struct literallyxcodetestlolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
